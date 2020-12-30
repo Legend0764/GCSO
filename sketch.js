e2d7c4c1c47dfc6fc168f0ec880a35bd6741ab06
Legend0764 <72258911+Legend0764@users.noreply.github.com>
2020-12-30 20:49:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var car,wall
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+car =   createSprite()
+
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
